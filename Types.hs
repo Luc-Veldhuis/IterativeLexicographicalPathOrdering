1f5742cb803bd6bd606963a7cd71396a5448de50
Luc Veldhuis <jfveldhuis@outlook.com>
2016-07-03 17:43:34 +0200

    add enters

--- Types.hs
+++ Types.hs
@@ -2,7 +2,11 @@
 module Types where
     import Data.Rewriting.Rule as Rule
 
-    data FunctionSymbol f = FunctionSymbol {name:: f, arity :: Int, star :: Bool} deriving (Show, Eq, Ord, Read)
+    data FunctionSymbol f = FunctionSymbol {
+        name:: f, 
+        arity :: Int, 
+        star :: Bool
+    } deriving (Show, Eq, Ord, Read)
     (-->) = Rule
     class (Eq v, Ord v, Show v) => EOS v
     instance EOS Int
